017b6fba083dfb6866ab724b88ccc8698bb1f460
robertanb <robertanb@localhost>
2020-07-26 14:43:55 -0300

    1 commit

---
+++ src/com/exampleJavaServlet/acao/ListaEmpresas.java
@@ -0,0 +1,27 @@
+package com.exampleJavaServlet.acao;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.exampleJavaServlet.modelo.Banco;
+import com.exampleJavaServlet.modelo.Empresa;
+
+public class ListaEmpresas implements Acao{
+	
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("listando empresas");
+		
+		Banco banco = new Banco();
+		List<Empresa> lista = banco.getEmpresas();
+		
+		request.setAttribute("empresas", lista);
+		
+		return "forward:listaEmpresas.jsp";
+	}
+
+}
---
+++ src/com/exampleJavaServlet/acao/RemoveEmpresa.java
@@ -0,0 +1,28 @@
+package com.exampleJavaServlet.acao;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.exampleJavaServlet.modelo.Banco;
+
+public class RemoveEmpresa  implements Acao {
+
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		System.out.println("acao removendo empresa");
+		
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		System.out.println(id);
+		
+		Banco banco = new Banco();
+		banco.removeEmpresa(id);
+		
+		return "redirect:entrada?acao=ListaEmpresas";
+	
+	}
+}
